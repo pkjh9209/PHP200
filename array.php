8ea6abae8120ef88e73aba60294fcf1705e8d35b
parkjunghyun <pkjh20177@gmail.com>
2020-09-21 22:24:57 +0900

    array

---
+++ array.php
@@ -0,0 +1,23 @@
+<?php
+
+ $space = array();
+ $space[0] = 'array01';
+ echo $space[0];
+ echo '<br>';
+
+//문자형 배열
+$earth = array();
+$earth['naton'] = 'korea';
+echo '문자형 배열 테스트 값은'.$earth['naton'].'입니다';
+echo '<br>';
+
+//화살표 형식
+$earth = array('test' => 'space');
+echo '화살표 배열'.$earth['test'].'입니다';
+echo '<br>';
+
+//반올림
+$roundTest01 = round(1542,-2);
+echo '반올림값'.$roundTest01;
+
+?>
